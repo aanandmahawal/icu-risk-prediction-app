2dcd0cb9b50e75d66583b814c6a47e0d5de51506
aanandmahawal <151204835+aanandmahawal@users.noreply.github.com>
2025-07-09 18:54:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 import joblib
 from predictor import streamlit_predict_custom_input
 
-MODEL_PATH = "tabpfn_model.pkl.gz"
+MODEL_PATH = "xgb_icu_model.pkl.gz"
 
 # ------------------------------
 # CSS: Minimal padding
